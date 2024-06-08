c6e933ab6165c3520e623f18a4690ba153822c04
lam <lam>
2024-06-08 23:34:01 +0700

    a

--- backend/src/models/course/GameModel.js
+++ backend/src/models/course/GameModel.js
@@ -4,20 +4,9 @@ const { Schema } = mongoose;
 // Define the Course schema
 const gameSchema = new Schema(
   {
-    unitId: { type: String, ref: "Unit", required: true },
-    category: {
-      type: String,
-      required: true,
-      enum: ["Từ vựng", "Ngữ âm", "Mẫu câu"],
-    },
-    name: { type: String, required: true },
-    videoURL: { type: String, required: false },
-    games: [
-      {
-        gameName: { type: String },
-        materialId: { type: String, ref: "Material" },
-      },
-    ],
+    lessonId: { type: String, ref: "Lesson", required: true },
+    gameName: { type: String },
+    materialId: { type: String, ref: "Material" },
   },
   { collection: "games" }
 );
